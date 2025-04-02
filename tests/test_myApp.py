219c063f7c68e366100b0947beab9762eb918c51
Olzhas Kurmangaliev <olzhaskurmangaliev335@gmail.com>
2025-04-02 16:12:45 +0500

    changed file path for tests

--- tests/test_myApp.py
+++ tests/test_myApp.py
@@ -1,5 +1,8 @@
-from ..myApp import app  
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
+from myApp import app
 def test_hello_world():
     client = app.test_client()  
     response = client.get('/')  
