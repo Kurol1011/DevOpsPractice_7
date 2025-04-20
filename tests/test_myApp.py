b9edab303ac786ade8620045eaeb67da09e655c0
Olzhas.Kurmangaliev <olzhaskurmangaliev335@gmail.com>
2025-04-20 16:16:46 +0500

    delete 2 files

--- tests/test_myApp.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-
-from myApp import app
-def test_hello_world():
-    client = app.test_client()  
-    response = client.get('/')  
-    assert response.data == b'Hello, World!'  
