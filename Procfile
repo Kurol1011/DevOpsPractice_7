3240bbb768348f5ccab88cc20294abcb318a2521
Olzhas.Kurmangaliev <olzhaskurmangaliev335@gmail.com>
2025-04-20 16:34:10 +0500

    изменил порт и хост

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: python myApp.py
--- myApp.py
+++ myApp.py
@@ -1,6 +1,4 @@
-import os
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,5 +6,4 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
